6220795a94aae174e6c2aa178b237fcdf2739f1f
Jean-Francis Bastien <jfbastien@petalmd.com>
2025-02-20 07:35:29 -0500

    r-on:click use instance_eval

--- components/component.rb
+++ components/component.rb
@@ -23,12 +23,7 @@ class Component
     JS.global[:document].getElementById(component.component_id).querySelectorAll('[r-on\\:click]').to_a.each do |button|
       button.addEventListener('click') do |_event|
         args = button.getAttribute('r-on:click').to_s
-        if args.start_with?('[')
-          method_name, *args = JSON.parse(args)
-          component.public_send(method_name, *args)
-        else
-          component.public_send(args)
-        end
+        component.instance_eval(args)
       end
     end
   end
