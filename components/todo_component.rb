bfb7bc214b7aa363d839d84b7c09684012c431b1
Jean-Francis Bastien <jfbastien@petalmd.com>
2025-02-26 21:39:48 -0500

    todo with filter

--- components/todo_component.rb
+++ components/todo_component.rb
@@ -8,23 +8,45 @@ class TodoComponent < Component
 
   attr_reactive :todos
   attr_reactive :new_todo
+  attr_reactive :filter
 
   def initialize(todos: [])
+    @all_todos = []
     @todos = todos
+    @filter = :all
   end
 
   def add_todo
-    self.todos += [Todo.new(SecureRandom.uuid, new_todo, false)]
+    @all_todos += [Todo.new(SecureRandom.uuid, new_todo, false)]
+    apply_filter(filter)
   end
 
   def toggle_completed(uuid)
     todo = todos.detect { |t| t.uuid == uuid }
     todo.completed = !todo.completed
-    self.todos = todos
+    @all_todos = todos
+    apply_filter(filter)
   end
   
   def remove_todo(uuid)
-    self.todos = todos.reject { |todo| todo.uuid == uuid }
+    @all_todos = todos.reject { |todo| todo.uuid == uuid }
+    apply_filter(filter)
+  end
+
+  def selected(filter_selected)
+    filter == filter_selected ? 'selected' : ''
+  end
+
+  def apply_filter(filter_selected)
+    self.filter = filter_selected
+    case filter
+      when :all
+        self.todos = @all_todos
+      when :completed
+        self.todos = @all_todos.select(&:completed)
+      when :active
+        self.todos = @all_todos.reject(&:completed)
+      end
   end
 
   def template
@@ -32,6 +54,13 @@ class TodoComponent < Component
       <h1>Todos</h1>
       <input type="text" r-model="new_todo">
       <button r-on:click="add_todo">Add Todo</button>
+      
+      <menu class="flex center">
+        <li class="<%= selected(:all) %>" r-on:click="apply_filter(:all)">All</li>
+        <li class="<%= selected(:active) %>" r-on:click="apply_filter(:active)">Active</li>
+        <li class="<%= selected(:completed) %>" r-on:click="apply_filter(:completed)">Completed</li>
+      </menu>
+      
       <% todos.reverse.each do |todo| %>
         <div data-key="<%= todo.uuid %>">
           <input 
